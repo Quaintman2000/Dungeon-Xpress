805d851d688860618e399c64c70a2260691fe56d
Zack Driscoll <zacdrisc@uat.edu>
2022-02-12 14:44:34 -0700

    Tried New Movement
    Tried to move using right click with mixed results.

--- Assets/Scripts/PlayerNavMesh.cs
+++ Assets/Scripts/PlayerNavMesh.cs
@@ -15,7 +15,35 @@ public class PlayerNavMesh : MonoBehaviour
 
     private void Update()
     {
-        if(Input.GetKeyDown(KeyCode.Space))
-            navMeshAgent.destination = movePosition.position;
+        if(Input.GetKeyDown(KeyCode.Mouse1))
+        {
+            MoveToClickPoint();
+        }
+    }
+
+    public void MoveToClickPoint()
+    {
+        //Create a plane object (a mathematical representation of all the points in 2D)
+        /*Plane groundPlane;
+
+        //Set that plane so it is the X,Z plane the plaer is standing on
+        groundPlane = new Plane(Vector3.up, transform.position);
+
+        //Cast a ray from our camera toward the plane, through our mouse cursor
+        float distance;*/
+        RaycastHit hit;
+        Ray cameraRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+        //groundPlane.Raycast(cameraRay, out distance);
+
+        //Find where that ray hits the plane
+        //Vector3 raycastPoint = cameraRay.GetPoint(distance);
+        
+        if (Physics.Raycast(cameraRay, out hit, Mathf.Infinity))
+        {
+            navMeshAgent.destination = hit.transform.position;
+        }
+
+        //Tell the NavMesh to go to the raycast point
+        //navMeshAgent.destination = raycastPoint;
     }
 }
