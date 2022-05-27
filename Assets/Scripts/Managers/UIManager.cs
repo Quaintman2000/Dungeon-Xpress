1fdbedd2316c60a4acc9e911101643d505a5d8ba
Alec <Quaintman2000@gmail.com>
2022-05-27 00:56:16 -0700

    Merging in Health/Mana UI

--- Assets/Scripts/Managers/UIManager.cs
+++ Assets/Scripts/Managers/UIManager.cs
@@ -2,81 +2,41 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
-using UnityEngine.Events;
 
-public class UIManager : MonoBehaviour
+public class UiManager : MonoBehaviour
 {
-    //standard variables and references
-    public static UIManager UIInstance;
-    [SerializeField] private PlayerController playerCtrl;
-    [SerializeField] private CombatController combatCtrl;
-    [SerializeField] private GameObject skillBar;
+    public static UiManager Instance;
+    //References for Bars
+    [SerializeField] private Image healthBar;
+    [SerializeField] private Image manaBar;
 
-    [Header("Abilities to assign")]
-    //the abilities that will be used everytime a button is clicked
-    [SerializeField] private AbilityData skill1_Ability;
-    [SerializeField] private AbilityData skill2_Ability;
-    [SerializeField] private AbilityData skill3_Ability;
-    [SerializeField] private AbilityData skill4_Ability;
-    
     void Awake()
     {
-        if(UIInstance == null)
+        if(Instance == null)
         {
-            UIInstance = this;
+            Instance = this;
         }
         else
         {
-            Destroy(UIInstance.gameObject);
-            UIInstance = this;
-        }
-
-        //set the bar unactive at start
-        skillBar.SetActive (false);
-    }
-
-    void Update()
-    {
-        //if player enters combat mode then show it
-        if(playerCtrl.currentState == CharacterController.PlayerState.InCombat)
-        {
-            skillBar.SetActive (true);
-        }
-        //if player is not in combat mode then hide it
-        else if (playerCtrl.currentState == CharacterController.PlayerState.FreeRoam)
-        {
-            skillBar.SetActive (false);
-            
+            Destroy(Instance.gameObject);
+            Instance = this;
         }
     }
 
-    /// Skills button and what they do each
-    public void Skill1()
+    //assign the bars to player accordingly
+    void Start ()
     {
-        if(combatCtrl)
-        {
-            combatCtrl.selectedAbilityData = skill1_Ability;
-        }
+        AssignHealthBar();
     }
-    public void Skill2()
+
+    //assign health bar of the player to its maximum value
+    public void AssignHealthBar()
     {
-        if(combatCtrl)
-        {
-            combatCtrl.selectedAbilityData = skill2_Ability;
-        }
+        healthBar.fillAmount = GameManager.Instance.playerData.Health / GameManager.Instance.playerData.classData.MaxHealth;
     }
-    public void Skill3()
+
+    void Update ()
     {
-        if(combatCtrl)
-        {
-            combatCtrl.selectedAbilityData = skill3_Ability;
-        }
-    }
-    public void Skill4()
-    {
-        if(combatCtrl)
-        {
-            combatCtrl.selectedAbilityData = skill4_Ability;
-        }
+
     }
 }
